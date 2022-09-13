271d7011ee8e8212d2e5a8079af60d92dda6e098
John Doe <johndoe@example.com>
2022-09-13 15:38:08 +0200

    Alap adatok felvétele, Scanner megírása

---
+++ src/hu/petrik/feladatok/Feladat14.java
@@ -0,0 +1,11 @@
+package hu.petrik.feladatok;
+
+import java.util.Scanner;
+
+public class Feladat14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Kérem adjon meg egy szöveget");
+        String szoveg = sc.nextLine();
+    }
+}
