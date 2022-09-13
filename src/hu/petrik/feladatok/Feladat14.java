7f9e06d9726dd322feec7879fbb5e441d185beb4
Morgován Nimród <morgovannimrod22@gmail.com>
2022-09-13 15:44:28 +0200

    menupontok megírása

--- src/hu/petrik/feladatok/Feladat14.java
+++ src/hu/petrik/feladatok/Feladat14.java
@@ -7,5 +7,15 @@ public class Feladat14 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Kérem adjon meg egy szöveget");
         String szoveg = sc.nextLine();
+        String menupont = "";
+        while (!menupont.equals("f")){
+            System.out.println("Adja meg, hogy mit szeretne csinálni a szöveggel:");
+            System.out.println("\ta - nagybetűssé alakítani");
+            System.out.println("\tb - kisbetűssé alakítani");
+            System.out.println("\tc - lekérdezni a hosszát");
+            System.out.println("\td - összehasonlítani egy másik szöveggel");
+            System.out.println("\te - a szöveg egy részét kiiratni");
+            System.out.println("\tf - kilépni");
+        }
     }
 }
